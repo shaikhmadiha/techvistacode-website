077dce26b42983bc275b1a191355309c37a045ff
faraz-github <farazahmad.email@gmail.com>
2025-08-15 11:59:16 +0530

    Stat build fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,4 @@
-import { Analytics } from "@vercel/analytics";
+import { Analytics } from "@vercel/analytics/next";
 
 import { Outfit } from "next/font/google";
 import { ThemeProvider } from "@mui/material";
